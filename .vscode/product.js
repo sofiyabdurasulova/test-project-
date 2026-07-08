fa70d7ba10d2b820e57c7aeee041eb739308be8b
sofiyabdurasulova <f1kaaa.2ooo@gmail.com>
2026-07-08 23:58:27 +0300

    test1

--- .vscode/product.js
+++ .vscode/product.js
@@ -1,11 +1,26 @@
 
-let cart = [];
-let buttons = document.querySelectorAll('.card__add-to-cart');
+const cart = [];
+const buttons = document.querySelectorAll('.card__add-to-cart');
 
 buttons.forEach(function (button) {
   button.addEventListener('click', function () {
-    let productName = button.closest('.card').querySelector('.card__title').textContent.trim();
+    const productName = button.closest('.card').querySelector('.card__title').textContent.trim();
     cart.push(productName);
     console.log("Товар добавлен в корзину:", cart);
   });
+});
+const cartButton = document.querySelector('.header__cart-button');
+cartButton.addEventListener('click', function () {
+  const cartModal = document.querySelector('.cart-modal');
+  cartModal.classList.remove('hidden');
+  console.log("Корзина открыта. Товары в корзине:", cart);
+});
+const catalogButton = document.querySelector('.cart-modal__button');
+const cartModal = document.querySelector('.cart-modal');
+catalogButton.addEventListener('click', function () {
+  const containerCards = document.querySelector('.container__cards');
+  console.log(containerCards);
+  cartModal.classList.add('hidden');
+  containerCards.scrollIntoView({ behavior: 'smooth' });
+  console.log("Переход к каталогу товаров.");
 });
