41b1ff283791518a9476c4fdab0616b8562d0abd
sofiyabdurasulova <f1kaaa.2ooo@gmail.com>
2026-07-09 17:14:21 +0300

    test2

--- test-project/product.js
+++ test-project/product.js
@@ -1,13 +1,37 @@
-
 const cart = [];
-const buttons = document.querySelectorAll('.card__add-to-cart');
-
-buttons.forEach(function (button) {
+const addToCartButtons = document.querySelectorAll('.card__add-to-cart');
+addToCartButtons.forEach(function (button) {
   button.addEventListener('click', function () {
-    const productName = button.closest('.card').querySelector('.card__title').textContent.trim();
-    cart.push(productName);
-    console.log("Товар добавлен в корзину:", cart);
+    const card = button.closest('.card');
+    const productName = card.querySelector('.card__title').textContent;
+    const productPrice = card.querySelector('.card__price').textContent;
+    const productImage = card.querySelector('.card__img').src;
+    cart.push({ name: productName, price: productPrice, image: productImage });
+    console.log("Товар добавлен в корзину:", { name: productName, price: productPrice, image: productImage });
   });
+});
+const cartModalEmpty = document.querySelector('.cart-modal__empty');
+const cartModalItems = document.querySelector('.cart-modal__items');
+if (cart.length === 0) {
+  cartModalEmpty.style.display = "flex"; // показать пустую корзину
+  cartModalItems.style.display = "none";
+} else {
+  cartModalEmpty.style.display = "none"; // скрыть пустую корзину
+  cartModalItems.style.display = "block";
+}
+cart.forEach(function (item) {
+  const cartItem = document.createElement('div');
+  cartItem.classList.add('cart-modal__item');
+  cartItem.innerHTML = `
+  <img class="cart-modal__item-img" src="${item.image}" alt="${item.name}">
+  <div class="cart-modal__item-title">${item.name}</div>
+  <div class="cart-modal__item-price">${item.price}</div>
+  <button class="cart-modal__remove"><i class="fa-solid fa-trash"></i></button>
+`;
+  cartItems.appendChild(cartItem);
+  cartItem.style.border = "2px solid red";
+  console.log(cartItems.innerHTML);
+  console.log("Товар отображен в корзине:", item);
 });
 const cartButton = document.querySelector('.header__cart-button');
 cartButton.addEventListener('click', function () {
@@ -23,4 +47,9 @@ catalogButton.addEventListener('click', function () {
   cartModal.classList.add('hidden');
   containerCards.scrollIntoView({ behavior: 'smooth' });
   console.log("Переход к каталогу товаров.");
+});
+const cartModalEmptyRemove = document.querySelector('.cart-modal__empty-remove');
+cartModalEmptyRemove.addEventListener('click', function () {
+  const card = document.querySelector('.card');
+  console.log(containerCards);
 });
